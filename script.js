99038e82a59f1c1f1955d7ee86499b208347c5f1
sarthakladhwe <sarthak.ladhwe156@gmail.com>
2022-06-21 17:23:29 +0530

    Displaying current time

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const imageAuthor = document.getElementById('image-author')
 const cryptoDiv = document.getElementById('crypto')
+const currentTimeEl = document.getElementById('current-time')
 
 fetch(`https://api.unsplash.com/photos/random?orientation=landscape&query=space`, {
     headers: {
@@ -18,7 +19,7 @@ fetch(`https://api.unsplash.com/photos/random?orientation=landscape&query=space`
         document.body.style.backgroundImage = `url("https://images.unsplash.com/photo-1519681393784-d120267933ba?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMzk3NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTU4MDQ1ODk&ixlib=rb-1.2.1&q=80&w=1080")`
     })
 
-fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currencyss=inr&ids=bitcoin,dogecoin,ethereum,litecoin`)
+fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&ids=bitcoin,dogecoin,ethereum,litecoin`)
     .then(res => {
         if(!res.ok) {
             throw Error("Something went wrong!")
@@ -32,12 +33,10 @@ fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currencyss=inr&ids=bitc
             divEl.classList.add("inner-crypto")
             divEl.innerHTML = `
                 <img src=${crypto.image} alt=${crypto.id} width="18px"/>
-                <p>${crypto.name} - ${crypto.current_price}₹</p>
+                <p>${crypto.name} - ₹${crypto.current_price}</p>
             `
             cryptoDiv.appendChild(divEl)
         }
-        
-        console.log(data)
     })
     .catch(err => {
         console.log(err)
@@ -46,3 +45,29 @@ fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currencyss=inr&ids=bitc
         divEl.style.fontSize = "12px"
         cryptoDiv.appendChild(divEl)
     })
+
+function getCurrentTime() {
+
+    const today = new Date();
+    let h = today.getHours() > 12 ? today.getHours() - 12 : today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    h = checkTime(h)
+    m = checkTime(m);
+    s = checkTime(s);
+    currentTimeEl.innerHTML =  h + " : " + m + " : " + s;
+    setTimeout(getCurrentTime, 1000);
+
+    // const currentTime = new Date().toLocaleTimeString()
+    // currentTimeEl.textContent = currentTime
+    // setTimeout(() => {
+    //     getCurrentTime()
+    // }, 1000)
+}   
+
+function checkTime(i) {
+    i < 10 && (i = "0" + i)  // add zero in front of numbers < 10
+    return i
+}
+
+getCurrentTime()
