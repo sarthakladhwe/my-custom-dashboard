7a2f433002ff0d66338da9231d7e1379fa848a14
sarthakladhwe <sarthak.ladhwe156@gmail.com>
2022-06-21 16:44:17 +0530

    Added Crypto API to get latest price of crypto

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-//client_id=F62xfIUzI0CFmQRGkxF6rbYlhxk20pxp11xvECFMVYg
-
 const imageAuthor = document.getElementById('image-author')
+const cryptoDiv = document.getElementById('crypto')
 
 fetch(`https://api.unsplash.com/photos/random?orientation=landscape&query=space`, {
     headers: {
@@ -14,4 +13,27 @@ fetch(`https://api.unsplash.com/photos/random?orientation=landscape&query=space`
         imageAuthor.textContent = `By: ${data.user.username}`
         imageAuthor.href = data.user.links.html
     })
+    .catch(err => {
+        console.log(err)
+        document.body.style.backgroundImage = `url("https://images.unsplash.com/photo-1519681393784-d120267933ba?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMzk3NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTU4MDQ1ODk&ixlib=rb-1.2.1&q=80&w=1080")`
+    })
 
+fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&ids=bitcoin,dogecoin,ethereum,litecoin`)
+    .then(res => res.json())
+    .then(data => {
+        for(let crypto of data) {
+            console.log(crypto)
+            const divEl = document.createElement('div')
+            divEl.classList.add("inner-crypto")
+            divEl.innerHTML = `
+                <img src=${crypto.image} alt=${crypto.id} width="18px"/>
+                <p>${crypto.name} - ${crypto.current_price}₹</p>
+            `
+            cryptoDiv.appendChild(divEl)
+        }
+        
+        console.log(data)
+    })
+    .catch(err => {
+        
+    })
